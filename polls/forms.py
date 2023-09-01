5e01c9693f38922794d06109692de76d857d2b93
Devyani030 <raidevyani3@gmail.com>
2023-09-01 11:28:59 +0530

    css editing

--- polls/forms.py
+++ polls/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class NameForm(forms.Form):
-    Your_Question = forms.CharField(max_length=100)
+    Your_Question = forms.CharField(max_length=500)
     Choice_1 = forms.CharField(max_length=100)
     Choice_2 = forms.CharField(max_length=100)
     Choice_3= forms.CharField(max_length=100)
